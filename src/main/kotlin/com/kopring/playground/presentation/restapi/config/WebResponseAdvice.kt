8ed23fb08ece43abe2ca9a9e4ebbb9ab4073b5a4
ybchar <uiurihappy@sju.ac.kr>
2024-05-04 17:42:52 +0900

    chore: GlobalResponse 추가

---
+++ src/main/kotlin/com/kopring/playground/presentation/restapi/config/WebResponseAdvice.kt
@@ -0,0 +1,45 @@
+package com.kopring.playground.presentation.restapi.config
+
+import com.kopring.playground.presentation.restapi.dto.GlobalResponse
+import com.kopring.playground.util.logger
+import org.springframework.core.MethodParameter
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.converter.HttpMessageConverter
+import org.springframework.http.server.ServerHttpRequest
+import org.springframework.http.server.ServerHttpResponse
+import org.springframework.http.server.ServletServerHttpResponse
+import org.springframework.web.bind.annotation.RestControllerAdvice
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice
+
+@RestControllerAdvice(basePackages = ["com.kopring.playground"])
+class WebResponseAdvice: ResponseBodyAdvice<Any> {
+    override fun supports(
+        returnType: MethodParameter,
+        converterType: Class<out HttpMessageConverter<*>>
+    ): Boolean {
+        return true
+    }
+
+    override fun beforeBodyWrite(
+        body: Any?,
+        returnType: MethodParameter,
+        selectedContentType: MediaType,
+        selectedConverterType: Class<out HttpMessageConverter<*>>,
+        request: ServerHttpRequest,
+        response: ServerHttpResponse
+    ): Any? {
+        val servletResponse =
+            (response as ServletServerHttpResponse).servletResponse
+        val status = servletResponse.status
+        val resolve = HttpStatus.resolve(status)
+        if (resolve == null || body is String) {
+            return body
+        }
+        if (resolve.is2xxSuccessful) {
+            return GlobalResponse.success(status, body)
+        }
+        return body
+    }
+
+}
